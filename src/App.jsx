2d4cd16cfdd2a5fc61a65b1e8635c0941634545d
behzod_709 <behzodsharifjonov7@gmail.com>
2024-09-28 10:02:08 +0500

    add main, detail file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Main from "./views/Main/Main"
+import Detail from "./views/Detail/Detail"
 
 
 function App() {
 
 
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="detail" element={<Detail />} />
+    </Routes>
+    </>
   )
 }
 
